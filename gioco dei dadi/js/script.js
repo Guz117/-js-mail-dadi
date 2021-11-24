f3339f25ca8b83c8310f24ea3668406629977e6e
Guz117 <cristian.guzman117@gmail.com>
2021-11-24 15:55:48 +0100

    semplificato gioco dei dadi

--- gioco dei dadi/js/script.js
+++ gioco dei dadi/js/script.js
@@ -3,17 +3,19 @@ Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 Stabilire il vincitore, in base a chi fa il punteggio più alto.
 */
 
-let generatorUser = [];
-let generatorPc = [];
-
-generatorUser.push(Math.floor((Math.random() * 6) + 1));
-generatorPc.push(Math.floor((Math.random() * 6) + 1));
+let generatorUser = (Math.floor((Math.random() * 6) + 1));
+let generatorPc = (Math.floor((Math.random() * 6) + 1));
 
 console.log(generatorUser);
 console.log(generatorPc);
 
 if (generatorUser > generatorPc) {
-    console.log('User Win')
+    console.log('User Win');
+    document.getElementById('winner').innerHTML = 'Utente Win';
 } else {
-    console.log('Pc Win')
+    console.log('Pc Win');
+    document.getElementById('winner').innerHTML = 'Pc Win';
 }
+
+document.getElementById('gamer').innerHTML = `Numero Utente= ${generatorUser}<br>
+Numero Pc= ${generatorPc}`
